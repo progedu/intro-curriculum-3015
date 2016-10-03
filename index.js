d4c06763b4198a9d29dd934e2b0c2d09fccfaa10
Yukimasa Shimada <github@eleos.jp>
2016-10-03 12:33:17 +0000

    アンケートに寿司とピザを追加しました。

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 'use strict';
 const http = require('http');
 const jade = require('jade');
+
+const survey = new Map(); // key:URL value:アンケート項目データのオブジェクト
+
+survey.set('/enquetes/yaki-shabu', {w1:'焼肉', w2:'しゃぶしゃぶ'});
+survey.set('/enquetes/rice-bread', {w1:'ごはん', w2:'パン'});
+survey.set('/enquetes/sushi-pizza', {w1:'すし', w2:'ピザ'});
+// survey.set('/enquetes/unko-curry', {w1:'ウ◯コ味のカレー', w2:'カレー味のウ◯コ'});
+
+
 const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'charset': 'utf-8'
+    'Content-Type': 'text/html; charset=UTF-8'　// 文字化け対策
+//    'charset': 'utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      if (req.url === '/enquetes/yaki-shabu') {
-        res.write(jade.renderFile('./form.jade', {
-          path: req.url,
-          firstItem: '焼き肉',
-          secondItem: 'しゃぶしゃぶ'
-        }));
-      } else if (req.url === '/enquetes/rice-bread') {
-        res.write(jade.renderFile('./form.jade', {
-          path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
-        }));
-      }
+
+		try {
+		const question = survey.get(req.url); // URLをキーにアンケート項目の単語を取得。
+		res.write(jade.renderFile('./form.jade', {
+					path: req.url,
+					firstItem: question.w1 ,
+					secondItem: question.w2
+				}));
+} catch (err){} 
+
       res.end();
       break;
     case 'POST':
