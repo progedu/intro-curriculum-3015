8843a591b8e11c3890e94c48454c0108de534ca6
kuchinashi-gadget <kuchinashi.gadget@gmail.com>
2017-02-14 09:03:53 +0000

    寿司とピザのアンケートの追加

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      }else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(jade.renderFile('./form.jade', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
+        }));
+      }else{
+        res.write(jade.renderFile('./form.jade', {
+          path: req.url,
+          firstItem: '牛丼',
+          secondItem: 'ラーメン'
+        }));
       }
       res.end();
       break;
