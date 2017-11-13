ddbf507e3ab275048393f3e649e5135a8a3b36c6
n4cl <devn4cl@gmail.com>
2017-11-14 01:48:27 +0900

    寿司orピザのアンケートを追加

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ const server = http.createServer((req, res) => {
           firstItem: '焼き肉',
           secondItem: 'しゃぶしゃぶ'
         }));
-      } else if (req.url === '/enquetes/rice-bread') {
+       } else if (req.url === '/enquetes/rice-bread') {
         res.write(jade.renderFile('./form.jade', {
           path: req.url,
           firstItem: 'ごはん',
           secondItem: 'パン'
+        }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(jade.renderFile('./form.jade', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
         }));
       }
       res.end();
