732547b9320c4c0f34fee2a11bf48f5d602e576b
elying <eri.tanaka213@gmail.com>
2017-11-20 16:29:34 +0900

    ピザと寿司のアンケートも追加しました

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
-      }
+      } else if (req.url === '/enquetes/sushi-pizza'){
+				res.write(jade.renderFile('./form.jade',{
+					path: req.url,
+					firstItem:　'寿司',
+					secondItem: 'ピザ'
+				}));
+			}
       res.end();
       break;
     case 'POST':
@@ -33,7 +39,7 @@ const server = http.createServer((req, res) => {
         body = Buffer.concat(body).toString();
         const decoded = decodeURIComponent(body);
         console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
+        res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
       });
@@ -51,3 +57,4 @@ const port = 8000;
 server.listen(port, () => {
   console.info('[' + new Date() + '] Listening on ' + port);
 });
+
