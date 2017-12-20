c38009fab14e14ab2f495441a6f1d9cab7c89841
AbeHaruhiko <caliconography@gmail.com>
2017-12-20 04:08:12 +0000

    寿司かピザかを選択するアンケートを実装

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ const server = http.createServer((req, res) => {
       } else if (req.url === '/enquetes/rice-bread') {
         res.write(jade.renderFile('./form.jade', {
           path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
+          firstItem: '焼き肉',
+          secondItem: 'しゃぶしゃぶ'
+        }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(jade.renderFile('./form.jade', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
         }));
       }
       res.end();
