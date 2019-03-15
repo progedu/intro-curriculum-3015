4b63deb81fceb9b16efa028a78d8925f90b2c784
taroimo5454 <number600boy@gmail.com>
2019-03-15 14:14:14 +0000

    究極の二択

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/dog-cat') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '犬',
+          secondItem: '猫'
+        }));
       }
       res.end();
       break;
