00a0da7a24fd52a485363cb150cee24785c3c1fb
Takeharu7-51 <take7spring@gmail.com>
2019-10-13 15:23:55 +0900

    アンケートにDQ派かFF派かを追加

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/dq-ff') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'ドラクエ',
+          secondItem: 'FF'
+        }));
       }
       res.end();
       break;
