fa27f9f94a8c0db7ee06df96d3e87927a02eff4e
Tee <milk06pop.12popken@gmail.com>
2020-01-24 23:39:45 +0900

    練習問題

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
-      }
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
+        }));
+    }
       res.end();
       break;
     case 'POST':
