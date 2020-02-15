8d54140bc88eb8c24b5fc9ee66b2bce1975fedd8
sackham <sackham0702@gmail.com>
2020-02-15 19:20:39 +0900

    sifue is zombi

--- index.js
+++ index.js
@@ -22,7 +22,14 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'すしざんまい',
+          secondItem: 'ピザハット'
+        }));
       }
+
       res.end();
       break;
     case 'POST':
