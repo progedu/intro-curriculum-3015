a518874d06680bc5834e8713c75e49475effa2a4
koume <koume138@gmail.com>
2020-03-27 23:20:26 +0900

    ピザかお寿司かアンケートを追加

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const server = http.createServer((req, res) => {
           firstItem: '焼き肉',
           secondItem: 'しゃぶしゃぶ'
         }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'ピザ',
+          secondItem: 'お寿司'
+        }));
       } else if (req.url === '/enquetes/rice-bread') {
         res.write(pug.renderFile('./form.pug', {
           path: req.url,
