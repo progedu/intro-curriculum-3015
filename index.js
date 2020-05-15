f82e348f09b8216fb42481b838357a02ad94833d
merururu <tbjys025@gmail.com>
2020-05-15 11:39:31 +0900

    カレーとパスタのアンケートにしました

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/curry-pasta') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'カレー',
+          secondItem: 'パスタ'
+        }));
       }
       res.end();
       break;
