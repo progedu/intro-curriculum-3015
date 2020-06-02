1e76792fc53f7dd0d562a71e45375219bb4028b0
aknkz <65381326+aknkz@users.noreply.github.com>
2020-06-02 11:12:32 +0900

    すしそばアンケートを追加

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/sushi-soba'){
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'すし',
+          secondItem: 'そば'
+        }));
       }
       res.end();
       break;
