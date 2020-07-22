dd7e6ff2a044c26998e8d0dce7f7a043961bf315
kayo-code <kayo_nagata@bita.jp>
2020-07-22 16:42:23 +0900

    test

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/sushi-pizza'){
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'すし',
+          secondItem: 'ピザ'
+        }));
       }
       res.end();
       break;
