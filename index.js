6d4cc071542e502aec2a731eb6bd2490ba7c4eb0
mumu\chacha <mutsuki.cocoa@gmail.com>
2020-08-29 17:32:55 +0900

    寿司かピザのアンケートを追加

--- index.js
+++ index.js
@@ -10,19 +10,23 @@ const server = http.createServer((req, res) => {
 
   switch (req.method) {
     case 'GET':
+      let firstItem = 'あんぱん';
+      let secondItem = 'クリームパン';
       if (req.url === '/enquetes/yaki-shabu') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '焼き肉',
-          secondItem: 'しゃぶしゃぶ'
-        }));
+          firstItem = '焼き肉';
+          secondItem = 'しゃぶしゃぶ';
       } else if (req.url === '/enquetes/rice-bread') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
-        }));
+          firstItem = 'ごはん';
+          secondItem = 'パン';
+      } else if (req.url === '/enquetes/sushi-pizza') {
+          firstItem = '寿司';
+          secondItem = 'ピザ';
       }
+      res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: firstItem,
+          secondItem: secondItem
+        }));
       res.end();
       break;
     case 'POST':
@@ -31,15 +35,15 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        console.info(`${now} 投稿: ` + decoded);
+        res.write(`<h1>${decoded} が投稿されました。</h1>`);
         res.end();
       });
       break;
     default:
       break;
   }
+  
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
