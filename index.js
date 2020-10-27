a99e06c438ce312ef9c217d6c9f1b13032c04200
vagrant <vagrant@vagrant.vm>
2020-10-27 20:29:03 +0900

    ハロウィン

--- index.js
+++ index.js
@@ -11,23 +11,24 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        if (req.url === '/enquetes/yaki-shabu') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '焼き肉',
-              secondItem: 'しゃぶしゃぶ'
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
-            })
-          );
-        }
+        let data = { path: req.url };
+        switch (req.url) {
+          case '/enquetes/yaki-shabu':
+            data[firstItem] = '焼き肉';
+            data[secondItem] = 'しゃぶしゃぶ';
+            break;
+          case '/enquetes/rice-bread':
+            data[firstItem] = 'ごはん';
+            data[secondItem] = 'パン';
+            break;
+          case '/enquetes/go-or-not':
+            data[firstItem] = 'ハロウィンで渋谷にいく';
+            data[secondItem] = '家で楽しむ';
+            break;
+          default:
+            break;
+        };
+        res.write(pug.renderFile('./form.pug'), data)
         res.end();
         break;
       case 'POST':
