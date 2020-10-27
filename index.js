e1ea741fbc652815aa7322895a4c119aaa5b31bd
eighgull <chmn7396@gmail.com>
2020-10-27 20:13:34 +0900

    テンプレートを使って新しいアンケートを追加

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const server = http
               secondItem: 'パン'
             })
           );
+        } else if(req.url === '/enquetes/mon-bull') {
+          res.write(
+            pug.renderFile('./form.pug' , {
+              path:req.url,
+              firstItem: 'モンスター',
+              secondItem: 'レッドブル'
+            })
+          );
         }
         res.end();
         break;
