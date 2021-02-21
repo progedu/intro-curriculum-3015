ff6903389bc1ce6e562b2cf1a64614bc29ef55ec
senarice <tarahukugohan@docomo.ne.jp>
2021-02-21 23:16:20 +0900

    すしぴざ実装2

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const server = http
   .on('clientError', e => {
     console.error('[' + new Date() + '] Client Error', e);
   });
-const port = 8001;
+const port = 8000;
 server.listen(port, () => {
   console.info('[' + new Date() + '] Listening on ' + port);
 });
